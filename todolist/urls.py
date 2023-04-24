748f6447c733ef2dfe1eff5fb0a83a7602580bb3
HYLLI <allaouainformatique@gmail.com>
2023-04-24 23:21:04 +0100

    adding form and delete option

--- todolist/urls.py
+++ todolist/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     
     path('',views.index,name='index'),
+    path('add',views.addTodoItem,name='add'),
+    path('completed/<todo_id>',views.completedTodo,name='completed'),
+    path('delete',views.delete,name='delete'),
+    path('deleteall',views.deleteAll,name='deleteall')
+
 ]
